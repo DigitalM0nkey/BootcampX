39bfbe037fb1493641071ee1c4e3f8b32e9154e0
Terrence Kuiper <github@terrencekuiper.com>
2019-10-17 05:40:09 +0000

    Protection from SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 const args = process.argv.slice(2);
+const cohortName = args[0];
+const values = [`%${cohortName}%`];
+
 
 const pool = new Pool({
   user: 'vagrant',
@@ -15,11 +18,12 @@ FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort} : ${user.teacher}`);
     });
-  });
+  })
+  .catch(err => console.error('query error', err.stack));
